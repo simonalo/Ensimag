4000ba291a6d3973c728ef2d76acc705ea2c85b6
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-03-25 16:26:43 +0100

    Ajout de auipc et de son test

--- lespinec_simonalo/program/autotest/auipc.s
+++ lespinec_simonalo/program/autotest/auipc.s
@@ -6,10 +6,10 @@
 	auipc x31, 0 
 
     ##Test d'addition d'un immédiat quelconque à pc (qui vaut maintenant à 0x1008 après avoir décodé auipc)
-    auipc x31, 0
+    auipc x31, 1234
 
 	# max_cycle 500
 	# pout_start
 	# 00001004 
-    # 00001008
+    # 12341008
 	# pout_end
