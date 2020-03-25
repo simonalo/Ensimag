703417ca422e7228b8bd63797f40372d82354cde
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-03-25 18:07:45 +0100

    Ajout de auipc et de son test

--- lespinec_simonalo/program/autotest/sub.s
+++ lespinec_simonalo/program/autotest/sub.s
@@ -12,10 +12,10 @@
 	lui x1, 0xfffff #Chargement d'une valeur maximale dans x1
     sub x31, x1, x31
 
-    #Test de soustraction de deux valeur quelconque dans x31
-    #lui x31, 0x12345 #Chargement d'une valeur nulle dans x31
-	#lui x1, 0x67890 #Chargement d'une valeur quelconque dans x1
-    #sub x31, x1, x31
+    #Test de soustraction de deux valeur quelconque dans x31 pour avoir un résultat négatif
+    lui x31, 0x00003 #Chargement d'une valeur nulle dans x31
+	lui x1, 0x00001 #Chargement d'une valeur quelconque dans x1
+    sub x31, x1, x31
 
 	# max_cycle 500
 	# pout_start
@@ -23,4 +23,6 @@
 	# 00000000 
 	# 00000000 
 	# FFFFF000
+	# 00003000
+	# 10001000
 	# pout_end
