df375f7de159e21446616bf067349de96c9da0e9
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-03-25 19:07:14 +0100

    Ajout de auipc et de son test

--- lespinec_simonalo/program/autotest/srl.s
+++ lespinec_simonalo/program/autotest/srl.s
@@ -5,13 +5,13 @@
     #Test de 1 décalage d'une valeur nulle dans x31
     lui x31, 0 #Chargement d'une valeur nulle dans x31
 	lui x1, 0 #Chargement d'une valeur nulle dans x1
-    srl x31, x1, 0x01
+    srl x31, x1, 1
 
     #Test de 2 décalages d'une valeur quelconque dans x31
     lui x31, 0 #Chargement d'une valeur nulle dans x31
 	lui x1, 0x12345 #Chargement d'une valeur maximale dans x1
     addi x31, x1, 0x678
-    srl x31, x31, 0x02
+    srl x31, x31, 2
 
 
 
