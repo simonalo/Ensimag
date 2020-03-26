275ebec64d1b673996b7e5493aa22a3b6ccf3525
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-03-26 08:01:08 +0100

    Autotest srai

--- lespinec_simonalo/program/autotest/srl.s
+++ lespinec_simonalo/program/autotest/srl.s
@@ -5,13 +5,13 @@
     #Test de 1 décalage d'une valeur nulle dans x31
     lui x31, 0 #Chargement d'une valeur nulle dans x31
 	lui x1, 0 #Chargement d'une valeur nulle dans x1
-	lui x2, 0x00001 #Chargement de 1 dans x2
+    addi x2, x1, 0x001 #Chargement de 1 dans x2
     srl x31, x2, x1
 
     #Test de 2 décalages d'une valeur quelconque dans x31
     lui x31, 0 #Chargement d'une valeur nulle dans x31
 	lui x1, 0 #Chargement d'une valeur nulle dans x1
-	lui x2, 0x00002 #Chargement de 2 dans x2
+    addi x2, x1, 0x002 #Chargement de 2 dans x2
     addi x31, x1, 0x123
     srl x31, x2, x31
 
