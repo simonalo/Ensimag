37bd9dd4ed2a7038fa8a632848ac14cc9f48faf6
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-03-26 07:54:38 +0100

    Autotest srai

--- lespinec_simonalo/program/autotest/sra.s
+++ lespinec_simonalo/program/autotest/sra.s
@@ -5,15 +5,15 @@
     #Test de 1 décalage d'une valeur nulle dans x31
     lui x31, 0 #Chargement d'une valeur nulle dans x31
 	lui x1, 0 #Chargement d'une valeur nulle dans x1
-	lui x2, 1 #Chargement d'une valeur nulle dans x1
-    sra x31, x1, x2
+	lui x2, 1 #Chargement de 1 dans x2
+    sra x31, x2, x1
 
     #Test de 2 décalages d'une valeur quelconque dans x31
     lui x31, 0 #Chargement d'une valeur nulle dans x31
-	lui x1, 0 #Chargement d'une valeur maximale dans x1
-	lui x2, 2 #Chargement d'une valeur maximale dans x1
+	lui x1, 0 #Chargement d'une valeur nulle dans x2
+	lui x2, 2 #Chargement de 2 dans x1
     addi x31, x1, 0x123
-    sra x31, x31, x2
+    sra x31, x2, x31
 
 
 	# max_cycle 500
--- lespinec_simonalo/program/autotest/srai.s
+++ lespinec_simonalo/program/autotest/srai.s
@@ -1,19 +1,17 @@
-# TAG = sra
+# TAG = srai
 
 	.text
 
     #Test de 1 décalage d'une valeur nulle dans x31
     lui x31, 0 #Chargement d'une valeur nulle dans x31
 	lui x1, 0 #Chargement d'une valeur nulle dans x1
-	lui x2, 1 #Chargement d'une valeur nulle dans x1
-    sra x31, x1, x2
+    sra x31, x1, 1
 
     #Test de 2 décalages d'une valeur quelconque dans x31
     lui x31, 0 #Chargement d'une valeur nulle dans x31
-	lui x1, 0 #Chargement d'une valeur maximale dans x1
-	lui x2, 2 #Chargement d'une valeur maximale dans x1
+	lui x1, 0 #Chargement d'une valeur nulle dans x2
     addi x31, x1, 0x123
-    sra x31, x31, x2
+    sra x31, x31, 2
 
 
 	# max_cycle 500
@@ -23,3 +21,4 @@
 	# 00000000  
 	# 00000123
 	# 00000048
+	# pout_end
