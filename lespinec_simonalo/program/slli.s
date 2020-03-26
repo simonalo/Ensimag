a956871aef28df13efd1c6ceb398e7392af925b3
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-03-26 07:58:41 +0100

    Autotest srai

--- lespinec_simonalo/program/slli.s
+++ lespinec_simonalo/program/slli.s
@@ -1,19 +1,17 @@
-# TAG = sra
+# TAG = srai
 
 	.text
 
     #Test de 1 décalage d'une valeur nulle dans x31
     lui x31, 0 #Chargement d'une valeur nulle dans x31
 	lui x1, 0 #Chargement d'une valeur nulle dans x1
-	lui x2, 0x00001 #Chargement de 1 dans x2
-    sra x31, x2, x1
+    sra x31, x1, 1
 
     #Test de 2 décalages d'une valeur quelconque dans x31
     lui x31, 0 #Chargement d'une valeur nulle dans x31
-	lui x1, 0 #Chargement d'une valeur nulle dans x1
-	lui x2, 0x00002 #Chargement de 2 dans x2
+	lui x1, 0 #Chargement d'une valeur nulle dans x2
     addi x31, x1, 0x123
-    sra x31, x2, x31
+    sra x31, x31, 2
 
 
 	# max_cycle 500
