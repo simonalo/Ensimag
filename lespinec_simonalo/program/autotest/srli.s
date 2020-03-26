998d7872a2855447305014fc22cc86a54bf89309
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-03-26 07:42:25 +0100

    Autotest srl

--- lespinec_simonalo/program/autotest/srli.s
+++ lespinec_simonalo/program/autotest/srli.s
@@ -1,19 +1,17 @@
-# TAG = srl
+# TAG = srli
 
 	.text
 
     #Test de 1 décalage d'une valeur nulle dans x31
     lui x31, 0 #Chargement d'une valeur nulle dans x31
 	lui x1, 0 #Chargement d'une valeur nulle dans x1
-	lui x2, 1 #Chargement d'une valeur nulle dans x1
-    srli x31, x1, x2
+    srli x31, x1, 1
 
     #Test de 2 décalages d'une valeur quelconque dans x31
     lui x31, 0 #Chargement d'une valeur nulle dans x31
 	lui x1, 0 #Chargement d'une valeur maximale dans x1
-	lui x2, 2 #Chargement d'une valeur maximale dans x1
     addi x31, x1, 0x123
-    srli x31, x31, x2
+    srli x31, x31, 2
 
 
 
