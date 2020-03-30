eb426de7889ef9cc26714b794b59c0501c6a43f5
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-03-30 15:31:38 +0200

    Conception de SLTI, SLTU et SLTIU

--- lespinec_simonalo/program/autotest/slti.s
+++ lespinec_simonalo/program/autotest/slti.s
@@ -10,7 +10,7 @@
 
     li x31, 0 #Chargement d'une valeur nulle dans x31
 	li x1, 0 #Chargement d'une valeur nulle dans x1
-    slti x31,x1, -2048
+    slti x31,x1, 1
 
 	# max_cycle 500
 	# pout_start
