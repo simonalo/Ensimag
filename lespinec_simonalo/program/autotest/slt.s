3b08ccb93e9be545003fbb75cdb80ed36c63766f
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-03-30 13:54:26 +0200

    Conception de SLT

--- lespinec_simonalo/program/autotest/slt.s
+++ lespinec_simonalo/program/autotest/slt.s
@@ -5,17 +5,17 @@
     #Test de 1 décalage d'une valeur nulle dans x31
     lui x31, 0 #Chargement d'une valeur nulle dans x31
 	lui x1, 0 #Chargement d'une valeur nulle dans x1
-	lui x1, 0 #Chargement d'une valeur nulle dans x2
+	lui x2, 0 #Chargement d'une valeur nulle dans x2
     slt x31,x1, x2 
 
     #Test de 2 décalages d'une valeur quelconque dans x31
     lui x31, 0 #Chargement d'une valeur nulle dans x31
 	lui x1, 0 #Chargement d'une valeur nulle dans x1
-	lui x1, 1 #Chargement de 1 dans x2
+	lui x2, 1 #Chargement de 1 dans x2
     slt x31,x1, x2 
 
 
-	# max_cycle 5000
+	# max_cycle 500
 	# pout_start
 	# 00000000 
 	# 00000000 
