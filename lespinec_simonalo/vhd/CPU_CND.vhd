7ed0c26ee5265a29f067cb89ee52bd3de7dc97ef
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-03-30 12:04:17 +0200

    Conception de CPU_CND

--- lespinec_simonalo/vhd/CPU_CND.vhd
+++ lespinec_simonalo/vhd/CPU_CND.vhd
@@ -28,7 +28,7 @@ begin
 
     -- On calcule la différence des deux vecteurs sur 33 bits
 
-    result <= (rs1(32) & rs1) - (alu_y(32) & alu_y) when (a = '1') else result := ('0' & rs1) - ('0' & alu_y);
+    result <= (rs1(32) & rs1) - (alu_y(32) & alu_y) when (a = '1') else ('0' & rs1) - ('0' & alu_y);
 
     -- On fait le calcule x - y et on atttribut les bonnes valeurs à z et s
     z <= '1' when (result = zeros) else '0';
