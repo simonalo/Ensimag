7c45eaa1e0db2ec27cef929553ae13c3bafcbfae
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-04-08 15:06:20 +0200

    Modification du test srl

--- lespinec_simonalo/program/autotest/srl.s
+++ lespinec_simonalo/program/autotest/srl.s
@@ -2,26 +2,42 @@
 
 	.text
 
-    #Test de 1 décalage d'une valeur nulle dans x31
-    lui x31, 0 #Chargement d'une valeur nulle dans x31
-	lui x1, 0 #Chargement d'une valeur nulle dans x1
-    addi x2, x1, 0x001 #Chargement de 1 dans x2
-    srl x31, x1, x2
+    #Test de un décalage d'une valeur nulle
+    li x1, 0x001 
+    srl x31, x0, x1
 
-    #Test de 2 décalages d'une valeur quelconque dans x31
-    lui x31, 0 #Chargement d'une valeur nulle dans x31
-	lui x1, 0 #Chargement d'une valeur nulle dans x1
-    addi x2, x1, 0x002 #Chargement de 2 dans x2
-    addi x31, x1, 0x123
-    srl x31, x31, x2
+	#Test de un décalage d'une valeur maximale
+	li x1, 0x001 
+    li x2, 0xfffffff 
+    srl x31, x2, x1
+
+    #Test de zéro décalages d'une valeur quelconque
+    li x1, 0x001
+	li x2, 0x12345678
+    srl x31, x2, x1
+
+	#Test de un décalages d'une valeur quelconque
+    li x1, 0x001
+	li x2, 0x12345678
+    srl x31, x2, x1
+
+	#Test de 8 décalages d'une valeur quelconque
+    li x1, 0x01f
+	li x2, 0x12345678
+    srl x31, x2, x1
+
+	#Test du maximum décalages d'une valeur quelconque
+    li x1, 0x01f
+	li x2, 0x12345678
+    srl x31, x2, x1
 
 
 
 	# max_cycle 500
 	# pout_start
 	# 00000000 
-	# 00000000 
-	# 00000000 
-	# 00000123
-	# 00000048
+	# 12345678
+	# 01234567
+	# 00000000
+	# 00000000
 	# pout_end
