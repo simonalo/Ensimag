30bcddbc4c376900fc5e0e9364d7b638a6cb420c
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-04-08 13:26:00 +0200

    Modifications test addi

--- lespinec_simonalo/program/autotest/addi.s
+++ lespinec_simonalo/program/autotest/addi.s
@@ -10,15 +10,15 @@
     addi x31, x1, 0
 
     #Test d'addition d'un registre nul et d'un immédiat de valeur maximale
-    addi x31, x0, 0xfff
+    addi x31, x0, 4095
 
 	#Test d'addition d'un registre  et d'un immédiat pour obtenir la valeur maximale
 	li x1, 0xfffff00f #Chargement d'une valeur maximale dans x1
-    addi x31, x1, 0xff0
+    addi x31, x1, 4080
 
 	#Test d'addition d'un registre  et d'un immédiat de valeur quelconques
 	li x1, 0x12345678 #Chargement d'une valeur maximale dans x1
-    addi x31, x1, 0x123
+    addi x31, x1, 291
 
 	# max_cycle 500
 	# pout_start
