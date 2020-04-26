ac74570f83577eb11dffe88ec15ffb1876679da4
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-04-26 17:45:59 +0200

    divu

--- lespinec_simonalo/vhd/CPU_PO.vhd
+++ lespinec_simonalo/vhd/CPU_PO.vhd
@@ -69,6 +69,7 @@ architecture RTL of CPU_PO is
     signal ALU_y, ALU_res : w32;
     -- Signal pour le deuxième UAL (multiplication, division, reste)
     signal ALU2_res_temp : signed(63 downto 0);
+    signal ALU2_res_temp2 : signed(32 downto 0);
     signal ALU2_res : w32;
     -- Signaux pour les opérations logiques
     signal LOGICAL_res : w32;
@@ -290,12 +291,15 @@ begin
                      (signed('0' & RS1_q) * signed('0' & ALU_y)) when cmd.ALU2_op_type = ALU_mulhu  else
                      (others => 'U');
 
+    ALU2_res_temp2 <= signed('0' & RS1_q) / signed('0' &  ALU_y) when cmd.ALU2_op_type = ALU_divu  else
+                     (others => 'U');
+
     ALU2_res <= unsigned(ALU2_res_temp(31 downto 0)) when cmd.ALU2_op_type = ALU_mul  else
                 unsigned(ALU2_res_temp(63 downto 32)) when cmd.ALU2_op_type = ALU_mulh  else
                 unsigned(ALU2_res_temp(63 downto 32)) when cmd.ALU2_op_type = ALU_mulhsu  else
                 unsigned(ALU2_res_temp(63 downto 32)) when cmd.ALU2_op_type = ALU_mulhu  else
                 unsigned(signed(RS1_q) / signed(ALU_y)) when cmd.ALU2_op_type = ALU_div  else
-                unsigned(signed('0' & RS1_q) / signed('0' &  ALU_y))(31 downto 0) when cmd.ALU2_op_type = ALU_div  else
+                unsigned(ALU2_res_temp2(31 downto 0)) when cmd.ALU2_op_type = ALU_divu  else
                 (others => 'U');
 
     -- Sélection de l'opération effectuée par l'opérateur logique
