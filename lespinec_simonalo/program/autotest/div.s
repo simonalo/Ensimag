36e02b99bed826b83c4598a560dcd9c2b55577fa
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-04-26 17:56:39 +0200

    div

--- lespinec_simonalo/program/autotest/div.s
+++ lespinec_simonalo/program/autotest/div.s
@@ -8,11 +8,11 @@
 
 	li x1, 0x80000000
 	li x2, 0x00000002
-    mul x31, x2, x1
+    mul x31, x1, x2
 
 	li x1, 0x80000000
 	li x2, 0x80000000
-    mul x31, x2, x1
+    mul x31, x1, x2
 
 	# max_cycle 500
 	# pout_start
