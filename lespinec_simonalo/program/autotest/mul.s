e55162c2de8d90b6cf436eba4c68de0baf441df4
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-04-26 12:18:17 +0200

    mul

--- lespinec_simonalo/program/autotest/mul.s
+++ lespinec_simonalo/program/autotest/mul.s
@@ -10,17 +10,17 @@
 	#Test de soustraction de deux valeur maximale dans x31
 	li x1, 0x00000001
 	li x2, 0x00000001
-    sub x31, x2, x1
+    mul x31, x2, x1
 
 	#Test de soustraction de deux valeur maximale dans x31
 	li x1, 0x00000003
 	li x2, 0x00000002
-    sub x31, x2, x1
+    mul x31, x2, x1
 
 	#Test de soustraction de deux valeur maximale dans x31
 	li x1, 0x0000F000
 	li x2, 0x00000002
-    sub x31, x2, x1
+    mul x31, x2, x1
 
 	# max_cycle 500
 	# pout_start
