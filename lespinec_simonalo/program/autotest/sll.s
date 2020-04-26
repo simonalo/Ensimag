93c19b11e08e73dff2d2cc4fced8c3ef6e6718e2
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-04-26 17:05:28 +0200

    mul

--- lespinec_simonalo/program/autotest/sll.s
+++ lespinec_simonalo/program/autotest/sll.s
@@ -3,11 +3,11 @@
 	.text
 
     #Test de un décalage d'une valeur nulle
-    li x1, 0x001 
+    li x1, 0x00000001 
     sll x31, x0, x1
 
 	#Test de un décalage d'une valeur maximale
-	li x1, 0x001 
+	li x1, 0x00000001 
     li x2, 0xffffffff 
     sll x31, x2, x1
 
@@ -16,17 +16,17 @@
     sll x31, x2, x0
 
 	#Test de un décalages d'une valeur quelconque
-    li x1, 0x001
+    li x1, 0x00000001
 	li x2, 0x23456789
     sll x31, x2, x1
 
 	#Test de 8 décalages d'une valeur quelconque
-    li x1, 0x008
+    li x1, 0x00000008
 	li x2, 0x12345678
     sll x31, x2, x1
 
 	#Test du maximum décalages d'une valeur quelconque
-    li x1, 0x01f
+    li x1, 0x0000001f
 	li x2, 0x12345678
     sll x31, x2, x1
 
