9974a6ebca1fe28a5918fa142c69de2274900fe6
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-04-26 17:20:50 +0200

    sr

--- lespinec_simonalo/program/autotest/slli.s
+++ lespinec_simonalo/program/autotest/slli.s
@@ -3,27 +3,27 @@
 	.text
 
     #Test de un décalage d'une valeur nulle
-    slli x31, x0, 0x00000001
+    slli x31, x0, 1
 
 	#Test de un décalage d'une valeur maximale
     li x1, 0xffffffff 
-    slli x31, x1, 0x00000001
+    slli x31, x1, 1
 	
     #Test de zéro décalages d'une valeur quelconque
 	li x1, 0x12345678
-    slli x31, x1, 0x00000000
+    slli x31, x1, 0
 
 	#Test de un décalages d'une valeur quelconque
 	li x1, 0x23456789
-    slli x31, x1, 0x00000001
+    slli x31, x1, 1
 
 	#Test de 8 décalages d'une valeur quelconque
 	li x2, 0x12345678
-    slli x31, x1, 0x00000008
+    slli x31, x1, 8
 
 	#Test du maximum décalages d'une valeur quelconque
 	li x1, 0x12345678
-    slli x31, x1, 0x00000020
+    slli x31, x1, 31
 
 
 
