620b98d94ef30db14d47f2479d1478ef14f96c84
Aloïs Simon <alois.simon@grenoble-inp.org>
2020-04-30 11:21:43 +0200

    test

--- lespinec_simonalo/program/autotest/csrrw.s
+++ lespinec_simonalo/program/autotest/csrrw.s
@@ -15,20 +15,20 @@
 	
 	addi   x1, x0, 0x7ff            # autorise des interruptions venant du PLIC
 	addi   x1, x1, 1                # bit 11 = 0x800 => 0x7ff + 1, car constante 12 bits sign ́ee pour addi
-									# les deux lignes pr ́ec ́edentes  ́etant  ́equivalente `a li x1,0x800
-	csrrs  x0, mie, x1              # Machine Extern Interrupt Enable bit (MEIE de mie) `a 1
+									# les deux lignes précédentes  ́etant  ́équivalente à li x1,0x800
+	csrrs  x0, mie, x1              # Machine Extern Interrupt Enable bit (MEIE de mie) à 1
 	
 	addi   x2, x0, 0
 
 attente:
 	beq    x2, x0, attente          # tourne tant que x2 vaut 0
-	addi 	x31, x0, 0x100             # indique sur pout que l’on a recu et trait ́e l’interruption
+	addi 	x31, x0, 0x100             # indique sur pout que l’on a recu et traité l’interruption
 	j attente                       # boucle infinie
 
 traitant:
 	addi  x2, x0, 1                 # change x2 pour sortir de la boucle infinie
 	lui   x3, 0x0c200               # acquitte l’interruption dans le plic
-	addi  x3, x3, 4                 # les deux lignes sont  ́equivalentes `a li x3,0x0C200004
+	addi  x3, x3, 4                 # les deux lignes sont  ́equivalentes à li x3,0x0C200004
 	lw    x1, 0(x3)                 # par lecture de l’adresse 0x0c2000004
 	mret    
 
